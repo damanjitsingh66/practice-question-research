c76cb8107628e1b3e200cd9f00b3f835ba6d04bd
Damanjit <damanjitdamanjit88@gmail.com>
2025-12-30 19:33:05 +0530

    #30 dec 2

--- src/main/java/questions/arrays/medium/KadanesAlgorithm.java
+++ src/main/java/questions/arrays/medium/KadanesAlgorithm.java
@@ -16,25 +16,40 @@ public class KadanesAlgorithm {
 //        }
 //        System.out.println(max);
 //brute force
+//        int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+//        int n = arr.length;
+//
+//        int maxSum = Integer.MIN_VALUE;
+//
+//        for (int i = 0; i < n; i++) {
+//            for (int j = i; j < n; j++) {
+//                int sum = 0;
+//
+//                for (int k = i; k <= j; k++) {
+//                    sum += arr[k];
+//                }
+//
+//                maxSum = Math.max(maxSum, sum);
+//            }
+//        }
+//
+//        System.out.println("Maximum Subarray Sum = " + maxSum);
+
+        //better one
         int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
         int n = arr.length;
 
         int maxSum = Integer.MIN_VALUE;
 
         for (int i = 0; i < n; i++) {
+            int sum = 0;
             for (int j = i; j < n; j++) {
-                int sum = 0;
-
-                for (int k = i; k <= j; k++) {
-                    sum += arr[k];
-                }
-
+                    sum += arr[j];
                 maxSum = Math.max(maxSum, sum);
             }
         }
 
         System.out.println("Maximum Subarray Sum = " + maxSum);
-
 
 
 
