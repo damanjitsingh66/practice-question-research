56cbb7126deff7d30694f762438ff819c891ac43
Damanjit <damanjitdamanjit88@gmail.com>
2025-12-30 19:58:59 +0530

    #30 dec 3

--- src/main/java/questions/arrays/medium/KadanesAlgorithm.java
+++ src/main/java/questions/arrays/medium/KadanesAlgorithm.java
@@ -36,22 +36,56 @@ public class KadanesAlgorithm {
 //        System.out.println("Maximum Subarray Sum = " + maxSum);
 
         //better one
+//        int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+//        int n = arr.length;
+//
+//        int maxSum = Integer.MIN_VALUE;
+//
+//        for (int i = 0; i < n; i++) {
+//            int sum = 0;
+//            for (int j = i; j < n; j++) {
+//                    sum += arr[j];
+//                maxSum = Math.max(maxSum, sum);
+//            }
+//        }
+//
+//        System.out.println("Maximum Subarray Sum = " + maxSum);
+
         int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
-        int n = arr.length;
 
-        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
 
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) {
-                    sum += arr[j];
-                maxSum = Math.max(maxSum, sum);
+        int start = 0;
+        int ansStart = -1;
+        int ansEnd = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (sum == 0) {
+                start = i;
+            }
+
+            sum += arr[i];
+
+            if (sum > max) {
+                max = sum;
+                ansStart = start;
+                ansEnd = i;
+            }
+
+            if (sum < 0) {
+                sum = 0;
             }
         }
 
-        System.out.println("Maximum Subarray Sum = " + maxSum);
+        System.out.println("Maximum Subarray Sum = " + max);
+        System.out.print("Subarray = ");
 
-
-
+        for (int i = ansStart; i <= ansEnd; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
+
+
 }
