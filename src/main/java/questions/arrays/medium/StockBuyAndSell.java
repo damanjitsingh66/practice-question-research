bb208f8c82af0f2c0bf81c08eadad48358fd6797
Damanjit <damanjitdamanjit88@gmail.com>
2026-01-02 20:58:30 +0530

    #2 jan

---
+++ src/main/java/questions/arrays/medium/StockBuyAndSell.java
@@ -0,0 +1,27 @@
+package questions.arrays.medium;
+
+public class StockBuyAndSell {
+
+    public static void main(String[] args) {
+
+        // brute force approach
+        // int[] arr = {7,1,5,3,6,4};
+//        int[] arr = {7,6,4,3,1};
+//        int max  = 0;
+//        int diff =0;
+//        for(int i=0; i<arr.length;i++){
+//            for(int j=i+1; j<arr.length; j++){
+//                diff = arr[j] - arr[i];
+//                if(diff>max){
+//                    max = diff;
+//                }
+//            }
+//        }
+//        System.out.println("maximum profit is - " + max);
+//
+
+
+
+
+    }
+}
