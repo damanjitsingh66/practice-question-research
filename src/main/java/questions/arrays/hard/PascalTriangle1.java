773b304d9f63209e263dd041e10dabe37b2af39a
Damanjit <damanjitdamanjit88@gmail.com>
2026-02-25 20:47:51 +0530

    #my first hard solution self without help

--- src/main/java/questions/arrays/hard/PascalTriangle1.java
+++ src/main/java/questions/arrays/hard/PascalTriangle1.java
@@ -7,16 +7,45 @@ public class PascalTriangle1 {
 
     public static void main(String[] args) {
 
-        int rows = 5;
+        int rows = 10;
+        int row = 2;
+        int col =2;
         List<List<Integer>> resultantList = new ArrayList<>();
         for(int i =1; i<=rows;i++){
             List<Integer> subList = new ArrayList<>();
           for(int j=1; j<=i;j++){
-              subList.add(j);
+
+              if(j==1 || j==i){
+                  subList.add(1);
+              }
+              else{
+                  if(resultantList.isEmpty()){
+                      subList.add(j);
+                  }else{
+                      List<Integer> previousSublist  = resultantList.get(i-2);
+                      if(!previousSublist.isEmpty()){
+
+                          subList.add(previousSublist.get(j-2)+previousSublist.get(j-1));
+                      }
+                  }
+
+              }
+
           }
           resultantList.add(subList);
         }
 System.out.println(resultantList);
+
+        if(resultantList.size()==1){
+            System.out.println("number is - "+resultantList.get(row-1).get(col-1));
+        }else{
+            System.out.println("number is - "+resultantList.get(row-1).get(col-1));
+        }
+
+
     }
+    //my first hard solution self without help
+    //Time Complexity	O(n²)
+    //Space Complexity	O(n²)
 
 }
