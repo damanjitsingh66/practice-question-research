9b28a4f59e92cb82a14d3dfa11cc9fe483a7f320
sarbjit16@hotmail.co.uk <sarbjit16@hotmail.co.uk>
2026-03-03 17:39:57 +0530

    #brute force for calculation of three sum

--- src/main/java/questions/arrays/hard/ThreeSum.java
+++ src/main/java/questions/arrays/hard/ThreeSum.java
@@ -2,6 +2,7 @@ package questions.arrays.hard;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ThreeSum {
 
@@ -9,13 +10,12 @@ public class ThreeSum {
     //output  -  [[-1,-1,2],[-1,0,1]]
 
     public static void main(String[] args) {
-
         int[] nums = {-1,0,1,2,-1,-4};
 //        nums = [-1,0,1,2,-1,-4]
         int n = nums.length;
-
         // Total subsets = 2^n
         int totalSubsets = 1 << n; // 2^n
+        List<List<Integer>> resultantSubsets = new ArrayList<>();
 
         for (int mask = 0; mask < totalSubsets; mask++) {
             List<Integer> subset = new ArrayList<>();
@@ -30,13 +30,35 @@ public class ThreeSum {
             for(int i:subset){
                 sum = sum + i;
             }
-            if(sum == 0 && subset.size()==3){
-                System.out.println(subset);
-            }
 
+            if(sum == 0 && subset.size()==3){
+
+                if(resultantSubsets.isEmpty()){
+                    resultantSubsets.add(subset);
+                }
+                else{
+
+
+                        for(List<Integer> presentSubsets : resultantSubsets){
+                            int presentElementCount = 0;
+                            for(Integer element: subset) {
+
+                                for (Integer presentElement : presentSubsets) {
+
+                                    if (Objects.equals(element, presentElement)) {
+                                        presentElementCount++;
+                                    }
+                                }
+                            }
+                            if(presentElementCount!=3){
+                                resultantSubsets.add(subset);
+                            }
+
+                    }
+                }
+
+                }
         }
 
     }
-
-
 }
