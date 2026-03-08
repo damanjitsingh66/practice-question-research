4097da1ce4e0ed51be86369e66b5c3515d090986
Damanjit <damanjitdamanjit88@gmail.com>
2026-03-08 13:57:02 +0530

    #8 march

--- src/main/java/questions/arrays/hard/FourSumWithTarget.java
+++ src/main/java/questions/arrays/hard/FourSumWithTarget.java
@@ -6,30 +6,83 @@ import java.util.*;
 public class FourSumWithTarget {
 
     public static void main(String[] args) {
+//
+//        int[] arr = {1,0,-1,0,-2,2};
+//        int target = 0;
+//        Set<List<Integer>> result = new HashSet<>();
+//
+//        for(int i=0; i<arr.length; i++){
+//            for(int j=i+1; j<arr.length; j++){
+//
+//                for(int k=j+1; k<arr.length; k++){
+//
+//                    for(int l=k+1; l<arr.length; l++){
+//
+//                        long sum = arr[i]+ arr[j];
+//                        sum += arr[k];
+//                        sum += arr[l];
+//
+//                        if(sum==target){
+//                         List<Integer> subset = Arrays.asList(arr[i],arr[j],arr[k],arr[l]);
+//                         result.add(subset);
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        System.out.println(result);
+
+
 
         int[] arr = {1,0,-1,0,-2,2};
         int target = 0;
-        Set<List<Integer>> result = new HashSet<>();
+        int n = arr.length;
 
-        for(int i=0; i<arr.length; i++){
-            for(int j=i+1; j<arr.length; j++){
+        List<List<Integer>> res = threeSum(arr, n,target);
 
-                for(int k=j+1; k<arr.length; k++){
+        for (List<Integer> triplet : res) {
+            for (int num : triplet) System.out.print(num + " ");
+            System.out.println();
+        }
 
-                    for(int l=k+1; l<arr.length; l++){
+    }
 
-                        long sum = arr[i]+ arr[j];
-                        sum += arr[k];
-                        sum += arr[l];
+    public static List<List<Integer>> threeSum(int[] arr, int n, int target) {
+        // Sort the array
+        Arrays.sort(arr);
+        // Store final result
+        List<List<Integer>> ans = new ArrayList<>();
 
-                        if(sum==target){
-                         List<Integer> subset = Arrays.asList(arr[i],arr[j],arr[k],arr[l]);
-                         result.add(subset);
-                        }
-                    }
+        // First loop for first element
+        for (int i = 0; i < n; i++) {
+            // Skip duplicates for first element
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+
+            for(int j=i+1;j<n;j++) {
+                // Two pointers
+                if (j!=i+1 && arr[j] == arr[j - 1]) continue;
+                int left = j + 1, right = n - 1;
+
+                // Find pairs for current arr[i]
+                while (left < right) {
+                    int sum = arr[i] + arr[j]+ arr[left] + arr[right];
+
+                    if (sum == target) {
+                        ans.add(Arrays.asList(arr[i], arr[j],arr[left], arr[right]));
+                        left++;
+                        right--;
+
+                        // Skip duplicates for left
+                        while (left < right && arr[left] == arr[left - 1]) left++;
+                        // Skip duplicates for right
+                        while (left < right && arr[right] == arr[right + 1]) right--;
+                    } else if (sum < 0) left++;
+                    else right--;
                 }
             }
         }
-        System.out.println(result);
+        return ans;
     }
+
+
 }
