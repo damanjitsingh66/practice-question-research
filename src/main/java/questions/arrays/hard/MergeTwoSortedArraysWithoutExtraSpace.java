fb67469aba6070a70c4723e7f8897a72ca4af8cb
sarbjit16@hotmail.co.uk <sarbjit16@hotmail.co.uk>
2026-03-11 14:57:15 +0530

    #9 march

--- src/main/java/questions/arrays/hard/MergeTwoSortedArraysWithoutExtraSpace.java
+++ src/main/java/questions/arrays/hard/MergeTwoSortedArraysWithoutExtraSpace.java
@@ -5,33 +5,44 @@ import java.util.function.IntPredicate;
 
 public class MergeTwoSortedArraysWithoutExtraSpace {
 
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i = m - 1;      // last element in nums1
+        int j = n - 1;      // last element in nums2
+        int k = m + n - 1;  // last position of nums1
+
+        // compare elements from end
+        while (i >= 0 && j >= 0) {
+
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+
+            k--;
+        }
+
+        // if nums2 still has elements
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+
     public static void main(String[] args) {
 
         int[] nums1 = {0, 2, 7, 8, 0, 0, 0};
-        int[]  nums2 = {-7, -3, -1};
+        int[] nums2 = {-7, -3, -1};
 
-          //Output : [-5, -3, -2, 1, 4, 5, 8]
+        int m = 4; // valid elements in nums1
+        int n = 3; // elements in nums2
 
-        for(int i=0; i<nums2.length;i++){
+        merge(nums1, m, nums2, n);
 
-            int finalI = i;
-            if(Arrays.stream(nums1).noneMatch(x-> x== nums2[finalI])){
-                int count =0;
-                for(int j =0; j< nums1.length;j++){
-
-                    if(count==0 && nums1[j]==0){
-                        nums1[j] = nums2[i];
-                        count++;
-                    }
-
-                }
-
-                }
-
-
-        }
-
-Arrays.sort(nums1);
         System.out.println(Arrays.toString(nums1));
     }
 }
