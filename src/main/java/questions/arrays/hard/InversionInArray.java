912b211600441049fc0cc6db742cbfb11d76965a
sarbjit16@hotmail.co.uk <sarbjit16@hotmail.co.uk>
2026-03-12 16:29:46 +0530

    #12 march

---
+++ src/main/java/questions/arrays/hard/InversionInArray.java
@@ -0,0 +1,25 @@
+package questions.arrays.hard;
+
+import java.util.*;
+
+public class InversionInArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {4,3,2,1};
+        int count =0;
+        for(int i =0; i<arr.length; i++){
+            for(int j = i+1; j<arr.length;j++){
+                if(arr[j] < arr[i]){
+                    count++;
+                }
+            }
+        }
+
+       System.out.println("inversion sum :- "+ count);
+
+        // tc - O(N)^2
+        //sc - O(1)
+
+    }
+}
