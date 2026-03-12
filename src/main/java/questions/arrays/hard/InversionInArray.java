4a23b85a4253bf2ee78e9daee4566698b506c672
sarbjit16@hotmail.co.uk <sarbjit16@hotmail.co.uk>
2026-03-12 16:39:36 +0530

    #11 march

--- src/main/java/questions/arrays/hard/InversionInArray.java
+++ src/main/java/questions/arrays/hard/InversionInArray.java
@@ -6,7 +6,7 @@ public class InversionInArray {
 
     public static void main(String[] args) {
 
-        int[] arr = {4,3,2,1};
+        int[] arr = {5,3,2,1,4};
         int count =0;
         for(int i =0; i<arr.length; i++){
             for(int j = i+1; j<arr.length;j++){
