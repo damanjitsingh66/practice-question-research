549c34193eb2ebe1a52b99a5a28506999c4d3028
sarbjit16@hotmail.co.uk <sarbjit16@hotmail.co.uk>
2026-03-13 09:22:48 +0530

    #12 march

---
+++ src/main/java/questions/arrays/hard/ReversePairs.java
@@ -0,0 +1,24 @@
+package questions.arrays.hard;
+
+public class ReversePairs {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+        int[] arr = {1,3,2,3,1};
+        int count=0;
+        for(int i=0; i<arr.length; i++){
+
+            for(int j=i+1; j<arr.length;j++){
+
+                if(arr[i] > 2*arr[j]){
+                    count++;
+                }
+
+            }
+
+        }
+       System.out.println(count);
+    }
+
+}
