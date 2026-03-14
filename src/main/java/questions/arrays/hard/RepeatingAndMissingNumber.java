e33d25786ae48f03456bad9692041423f85037dc
Damanjit <damanjitdamanjit88@gmail.com>
2026-03-14 20:32:54 +0530

    #15 march

--- src/main/java/questions/arrays/hard/RepeatingAndMissingNumber.java
+++ src/main/java/questions/arrays/hard/RepeatingAndMissingNumber.java
@@ -1,6 +1,7 @@
 package questions.arrays.hard;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RepeatingAndMissingNumber {
@@ -10,38 +11,40 @@ public class RepeatingAndMissingNumber {
         int[] nums = {3,5,4,1,6,1,7};
         //output -{1,2}
 
-        int n = nums.length;
-        boolean[] visited = new boolean[n+1];
+        long n = nums.length;
 
-        List<Integer> result = new ArrayList<>();
+        // Sum of first n natural numbers
+        long SN = (n * (n + 1)) / 2;
 
-        int repeated = -1;
-        int missing = -1;
+        // Sum of squares of first n natural numbers
+        long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
 
-        for(int i=0; i<nums.length;i++){
-            if(visited[nums[i]]){
-                repeated = nums[i];
-            }
-            else{
-                visited[nums[i]] = true;
-            }
+        // Calculate actual sum (S) and sum of squares (S2) of array elements
+        long S = 0, S2 = 0;
+        for (int i = 0; i < n; i++) {
+            S += nums[i];
+            S2 += (long) nums[i] * (long) nums[i];
         }
-        for(int i=1; i<=n; i++){
-            if(!visited[i]){
-                missing = i;
-                break;
-            }
-        }
-        result.add(repeated);
-        result.add(missing);
 
-        System.out.println(result);
+        // Compute the difference values
+        long val1 = S - SN; // X - Y
 
+        // S2 - S2n = X^2 - Y^2
+        long val2 = S2 - S2N;
+
+        // Calculate X + Y
+        val2 = val2 / val1;
+
+        // Calculate X and Y
+        long x = (val1 + val2) / 2; // repeating
+        long y = x - val1;          // missing
+
+        System.out.println(Arrays.asList(x,y));
         //Complexity
 
         //Time: O(n)
 
-        //Space: O(n)
+        //Space: O(1)
 
     }
 }
