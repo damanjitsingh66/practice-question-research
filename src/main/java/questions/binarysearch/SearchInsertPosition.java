107e187948e609789f016a633d10e9443cc0a63d
sarbjit16@hotmail.co.uk <sarbjit16@hotmail.co.uk>
2026-03-18 14:58:40 +0530

    #march 18

--- src/main/java/questions/binarysearch/SearchInsertPosition.java
+++ src/main/java/questions/binarysearch/SearchInsertPosition.java
@@ -4,17 +4,25 @@ public class SearchInsertPosition {
 
     public static void main(String[] args) {
         int[] arr = {1,2,4,7};
-        int target = 3;
+        int target = 1;
         System.out.println("insert index element - "+ insertIndex(arr,target));
     }
     public static int insertIndex(int[] arr, int target){
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]>=target){
-                return i;
+        int low = 0;
+        int high = arr.length-1;
+        int ans = arr.length;
+        while(low<=high){
+            int mid = (low + high)/2;
+            if(arr[mid]>=target){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid + 1;
             }
         }
-        return arr.length;
+     return ans;
     }
-    //tc-O(N)
+    //tc-O(log n)
     //sc-O(1)
 }
