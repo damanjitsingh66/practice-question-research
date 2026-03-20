21ea6042752658a4b1e65b1753bd826986e05744
sarbjit16@hotmail.co.uk <sarbjit16@hotmail.co.uk>
2026-03-20 17:30:33 +0530

    #march 20

--- src/main/java/questions/binarysearch/OccurenceInArray.java
+++ src/main/java/questions/binarysearch/OccurenceInArray.java
@@ -1,20 +1,69 @@
 package questions.binarysearch;
 
+import java.util.List;
+
 public class OccurenceInArray {
     public static void main(String[] args) {
 
-        int[] arr = {3,2,3,3,3,3,4};
+        int[] arr = {1,2,3,3,3,3,4};
         int x= 3;
-        //output will be 4
-        int count  =0 ;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]== x){
-                count++;
-            }
-        }
-        System.out.println(count);
+      int firstOcc = FirstOccurrenceInArray(arr,x);
+      int lastOcc = LastOccurrenceInArray(arr, x);
+
+      if(firstOcc==-1 || lastOcc ==-1){
+
+      }
+
+
     }
+    public static int LastOccurrenceInArray(int[] arr, int target){
+        int low = 0;
+        int high = arr.length -1;
+        int ans = -1;
+
+        while(low<=high){
+
+            int mid = (low + high) / 2;
+            if(arr[mid]== target){
+               ans = mid;
+               low = mid + 1;
+            }
+            if(arr[mid]<target){
+                high = mid -1;
+            }
+            else{
+                low = mid + 1;
+            }
+
+        }
+        return ans;
+    }
+
+    public static int FirstOccurrenceInArray(int[] arr, int target){
+        int low = 0;
+        int high = arr.length -1;
+        int ans = -1;
+
+        while(low<=high){
+
+            int mid = (low + high) / 2;
+            if(arr[mid]== target){
+                ans = mid;
+                high = mid -1;
+            }
+            if(arr[mid]<target){
+                low = mid + 1;
+            }
+            else{
+                high = mid -1;
+            }
+
+        }
+        return ans;
+    }
+
+
+
+
 }
 
-//tc - O(N)
-//sc -O(1)
