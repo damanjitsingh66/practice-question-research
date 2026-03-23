9f26df40291573753d335353621945722fb3b967
sarbjit16@hotmail.co.uk <sarbjit16@hotmail.co.uk>
2026-03-23 17:13:37 +0530

    #march 23

---
+++ src/main/java/questions/binarysearch/SearchElementInRotatedSortedArray.java
@@ -0,0 +1,18 @@
+package questions.binarysearch;
+
+public class SearchElementInRotatedSortedArray {
+
+    public static void main(String[] args) {
+
+     int[] arr = {4, 5, 6, 7, 0, 1, 2};
+     //output - 4
+     int k = 0;
+     int low = 0;
+     int high = arr.length;
+
+
+
+    }
+
+
+}
