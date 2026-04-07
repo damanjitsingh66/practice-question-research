ecc3fdb33ccba19fe797f73c88c5a16192a69314
sarbjit16@hotmail.co.uk <sarbjit16@hotmail.co.uk>
2026-04-07 12:11:05 +0530

    #7 April

---
+++ src/main/java/questions/binarysearch/answer/SquarerootOfNumber.java
@@ -0,0 +1,17 @@
+package questions.binarysearch.answer;
+
+public class SquarerootOfNumber {
+
+    public static void main(String[] args) {
+
+        int n = 49;
+
+        System.out.println((int)Math.sqrt(n));
+
+        //tc- O(1)
+        //sc-O(1)
+
+    }
+
+}
+
