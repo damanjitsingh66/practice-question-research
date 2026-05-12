7a1f6330731487a2fe3d65c4a4ea4f3473a520cf
Damanjit <damanjitdamanjit88@gmail.com>
2026-05-12 07:31:59 +0530

    #12 may

--- src/main/java/questions/Strings/LargestOddNumberInString.java
+++ src/main/java/questions/Strings/LargestOddNumberInString.java
@@ -3,8 +3,8 @@ package questions.Strings;
 public class LargestOddNumberInString {
 
     public static void main(String[] args) {
-
-        String num = "5347";
+        int max = Integer.MIN_VALUE;
+        String num = "5424";
         int n = num.length();
         for(int i=0; i<n; i++){
             if(i==0){
@@ -12,23 +12,28 @@ public class LargestOddNumberInString {
           }else{
                 num = num.substring(1);
             }
-            System.out.println(num);
+            int numo = Integer.parseInt(num);
+           int largestNumber =  maxOddNumber(numo);
+           if(largestNumber>max){
+               max = largestNumber;
+           }
         }
-
-
-        int numo = Integer.parseInt(num);
-
-
-
-
-
+        System.out.println(max);
 
     }
 
-    public static void generatePairs(int numo){
+    public static int maxOddNumber(int numo){
+        int max = Integer.MIN_VALUE;
         while(numo > 0){
-            System.out.println(numo);
-            numo=numo /10;
+            if(numo>max){
+                if(numo%2!=0) {
+                    max = numo;
+                }
+            }
+                numo = numo / 10;
+
         }
+        return max;
     }
 }
+//mine wrong approach
