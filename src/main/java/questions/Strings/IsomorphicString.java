fb11c71633ec4601f012e04cbc6fd2f5c32064e4
sarbjit16@hotmail.co.uk <sarbjit16@hotmail.co.uk>
2026-05-26 12:33:47 +0530

    #26 may

---
+++ src/main/java/questions/Strings/IsomorphicString.java
@@ -0,0 +1,46 @@
+package questions.Strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicString {
+
+    public static void main(String[] args) {
+
+        String s = "aa";
+        String t = "ab";
+
+        System.out.println(isIsomorphic(s, t));
+    }
+
+    private static boolean isIsomorphic(String s, String t){
+
+        Map<Character, Character> assignmentMap = new HashMap<>();
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        for(int i = 0; i < s.length(); i++){
+
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if(assignmentMap.containsKey(c1)){
+
+                if(assignmentMap.get(c1) != c2){
+                    return false;
+                }
+
+            } else {
+
+                if(assignmentMap.containsValue(c2)){
+                    return false;
+                }
+
+                assignmentMap.put(c1, c2);
+            }
+        }
+        return true;
+    }
+}
