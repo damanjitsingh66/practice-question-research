ee11c05514512b1a8f72e958e1bbf3c52fe76d99
sarbjit16@hotmail.co.uk <sarbjit16@hotmail.co.uk>
2026-05-28 17:12:45 +0530

    #28 may

---
+++ src/main/java/questions/Strings/RotationOfString.java
@@ -0,0 +1,24 @@
+package questions.Strings;
+
+public class RotationOfString {
+    public static void main(String[] args) {
+        String s = "rotation";
+        String goal = "tionrota";
+        System.out.println(isRotatedString(s,goal));
+    }
+    public static boolean isRotatedString(String s, String goal){
+
+        if(s.length()!=goal.length()){
+            return false;
+        }
+        String res = s;
+        for(int i=0;i<s.length();i++){
+             res = res.substring(1);
+             res = res + s.charAt(i);
+             if(res.equals(goal)){
+                 return true;
+             }
+        }
+        return false;
+    }
+}
