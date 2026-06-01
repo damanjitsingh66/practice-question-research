36db957347807aba9483ef2397e363af11efdd63
sarbjit16@hotmail.co.uk <sarbjit16@hotmail.co.uk>
2026-06-01 17:05:33 +0530

    #correct solution

--- src/main/java/questions/Strings/MaximumNestedDepthOfParanthesis.java
+++ src/main/java/questions/Strings/MaximumNestedDepthOfParanthesis.java
@@ -3,42 +3,28 @@ package questions.Strings;
 public class MaximumNestedDepthOfParanthesis {
 
     public static void main(String[] args) {
-        String  s = "(1)+((2))+((3))";
-        //output - 3
-//        String s = "((()))";
+        String  s = "(1)+((2))+(((3)))";
 
 System.out.println(maximumNestedDepth(s));
     }
-
+//correct one
     public static int maximumNestedDepth(String s){
 
         int level = 0;
-        int count = 1;
-        int max = Integer.MIN_VALUE;
+        int max = 0;
 
         for(int i=0; i<s.length();i++){
 
             if(s.charAt(i)=='(') {
-                if(level>0) {
-                    count++;
-                }
                 level++;
             }
             else if(s.charAt(i)==')'){
-                 if(level ==0){
-                     count = 1;
-                 }
                 level--;
-                if(level>0) {
-                    count--;
-                }
-
             }
-            max = Math.max(count,max);
+            max = Math.max(level,max);
         }
-
-
         return max;
     }
-
+//tc- O(N)
+    //sc-O(1)
 }
